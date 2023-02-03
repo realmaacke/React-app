f280b836f0489700490a8e292fc69b782042a16f
realmaacke <mackan.pettersson1@outlook.com>
2023-02-03 22:57:51 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+function Nav(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <ul>
+        <li><a>Button</a></li>
+        <li><a>Button</a></li>
+      </ul>
+    </header>
   );
 }
 
-export default App;
+
+export {
+  Nav
+}
