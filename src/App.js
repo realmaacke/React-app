0b5e964d640863059ce8f22f7bc9bd1499c005e6
realmaacke <mackan.pettersson1@outlook.com>
2023-02-04 00:51:15 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import './code-editor.css';
+import './repos.css';
 
 function Nav(){
   return (
     <header>
       <ul>
-        <li><a>Button</a></li>
-        <li><a>Button</a></li>
+        <li> <a>Programing News</a> </li>
+        <li> <a>Repositorys</a> </li>
+        <li> <a>Online Code</a> </li>
+
       </ul>
     </header>
   );
 }
 
 
+
+function OnlineCode(){
+  return (
+
+    <div className='code-container'>
+        
+      <div className='editor'>
+          <textarea>
+
+          </textarea>
+
+          <button>Run code</button>
+
+      </div>
+
+
+      <div className='output'>
+
+        <div className='output-terminal'>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  );
+}
+
+
+function Repos(){
+
+  return (
+    <div className='repos-container'>
+
+      <div className='repositorys'>
+
+        <div className='repos-list'>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+
 export {
-  Nav
+  Nav,
+  OnlineCode,
+  Repos
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {Nav} from './App';
+import {Nav, OnlineCode, Repos} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Nav />
-  
+  <Repos />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
